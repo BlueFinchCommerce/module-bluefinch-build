7ce20cfac63194d6739bfa33115a18ee07d3cc43
Chloe <chloe@gene.co.uk>
2024-12-04 15:45:18 +0000

    origin/main

---
+++ ViewModel/Theme.php
@@ -0,0 +1,41 @@
+<?php
+namespace Gene\Exodus\ViewModel;
+
+use \Magento\Framework\View\Element\Block\ArgumentInterface;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ * Class GetStoreConfig
+ * @package Gene\Framework\ViewModel
+ */
+class Theme implements ArgumentInterface
+{
+
+    /**
+     * @var Image
+     */
+    protected $scopeConfig;
+
+    /**
+     * GetStoreConfig constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Get store config - pass through param 'section/group/field'
+     * @param $config
+     * @return mixed
+     */
+    public function getStoreConfig($config)
+    {
+        return $this->scopeConfig->getValue(
+            $config,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+        );
+    }
+}
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Gene_Exodus',
+    __DIR__);
